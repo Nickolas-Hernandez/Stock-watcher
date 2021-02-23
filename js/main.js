210f26ee9104e9400c15e8d9758d6d670dfd5b9c
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-23 15:28:25 -0800

    added event listener to watchlist page to listen for input event. #1

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+// Global variables
 var $watchListPage = document.querySelector('.watchlist-page');
+
+// Functions
+
+// Event Listeners + Function Calls
+$watchListPage.addEventListener('input', autoCompletedSuggest);
+
+// Request Functions
