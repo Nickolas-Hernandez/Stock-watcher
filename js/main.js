76e2ddbcbee3aa6580e7a2fd95b1895f9b0f3344
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-23 15:55:30 -0800

    working on creating function to send various requests to alpha vantage api. #1

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ var $watchListPage = document.querySelector('.watchlist-page');
 $watchListPage.addEventListener('input', autoCompletedSuggest);
 
 // Request Functions
+function sendRequestAlphaVantage(type, ticker, keyword){
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", `https://www.alphavantage.co/query?function=OVERVIEW&symbol=TSLA&apikey=CPOI5XYGUXDVNA28`);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function(){
+    console.log('status', xhr.status);
+    console.log('response', xhr.response);
+  });
+  xhr.send()
+}
