afe80ab21bd99971e94fb4cd05bcedbfc07cd750
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-23 17:34:35 -0800

    Created and displayed list of auto suggest items on search and removed that list when needed. #1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,41 @@
 // Global variables
-var $watchListPage = document.querySelector('.watchlist-page');
 var $searchInput = document.querySelector('#search-input');
+var $suggestionBox = document.querySelector('.auto-list');
 
 // Functions
+function autoCompleteSuggest(event){
+  if(event.target.value.length >= 3) {
+    sendRequestAlphaVantage('autocomplete', null, event.target.value);
+  }else{
+    $suggestionBox.classList.remove('active');
+    var currentItems = document.querySelectorAll('.auto-suggest-item');
+    for(var i = 0; i < currentItems.length; i++){
+      currentItems[i].remove();
+    }
+  }
+
+}
 
 // Event Listeners + Function Calls
-$searchInput.addEventListener('input', sendRequestAlphaVantage('autocomplete', null, event));
+$searchInput.addEventListener('input', autoCompleteSuggest);
 
 // Request Functions
-function sendRequestAlphaVantage(type, ticker, event){
+function sendRequestAlphaVantage(type, ticker, keyword){
   if(ticker !== null) ticker = ticker.toUpperCase();
   var xhr = new XMLHttpRequest();
   xhr.responseType = 'json';
   if(type === 'autocomplete'){
-    xhr.open("GET", `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${event.target.value}&apikey=CPOI5XYGUXDVNA28`);
+    xhr.open("GET", `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${keyword}&apikey=CPOI5XYGUXDVNA28`);
     xhr.addEventListener('load', function(){
       console.log('status', xhr.status);
       console.log('response', xhr.response);
+      for(var i = 0; i <xhr.response.bestMatches.length; i++){
+        $suggestionBox.classList.add('active')
+        var suggestionItem = document.createElement('li');
+        suggestionItem.className = 'auto-suggest-item';
+        suggestionItem.textContent = xhr.response.bestMatches[i]["1. symbol"];
+        $suggestionBox.appendChild(suggestionItem);
+      }
     });
   }else if( type === 'daily'){
   xhr.open("GET", `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${ticker}&apikey=CPOI5XYGUXDVNA28`);
