04093f2404bfd8afad7438ba95e13bb549c64525
Nick Hernandez <xnickhernandez@gmail.com>
2021-02-24 12:16:19 -0800

    Converted autocomplete to CNBC APIs autocomplete. #1

--- js/main.js
+++ js/main.js
@@ -2,17 +2,15 @@
 var $searchInput = document.querySelector('#search-input');
 var $suggestionBox = document.querySelector('.auto-list');
 var $searchIcon = document.querySelector('.fa-search');
-var dailyFunction = 'TIME_SERIES_DAILY';
-var overviewFunction = 'OVERVIEW';
-var autoSuggestFunction = 'SYMBOL_SEARCH';
 var trendingStoriesRequest = 'TRENDING';
-var symbolStoriesRequest = 'SYMBOL';
+var companyNewsRequest = 'SYMBOL';
+var autoCompleteRequest = 'AUTO'
 
 // Functions
 function autoCompleteSuggest(event) {
   removeSuggestionList();
   if (event.target.value.length >= 3) {
-    sendRequestAlphaVantage(autoSuggestFunction, null, event.target.value);
+    sendRequestCNBC(autoCompleteRequest, null, event.target.value);
     $suggestionBox.classList.add('active');
   }
 }
@@ -37,11 +35,13 @@ function submitSearch(event) {
   $searchInput.value = '';
 }
 
-function createAutoSuggestItem(string) {
-  var suggestionItem = document.createElement('li');
-  suggestionItem.className = 'auto-suggest-item';
-  suggestionItem.textContent = string;
-  $suggestionBox.appendChild(suggestionItem);
+function createAutoSuggestItem(response) {
+  for(var i = 1; i < response.length; i++){
+    var suggestionItem = document.createElement('li');
+    suggestionItem.className = 'auto-suggest-item';
+    suggestionItem.textContent = response[i].symbolName;
+    $suggestionBox.appendChild(suggestionItem);
+  }
 }
 
 function getTrendingStories(event) {
@@ -49,47 +49,31 @@ function getTrendingStories(event) {
 }
 
 // Request Functions
-function sendRequestAlphaVantage(functionType, ticker, keyword) {
-  if (ticker !== null) ticker = ticker.toUpperCase();
-  var xhr = new XMLHttpRequest();
-  xhr.responseType = 'json';
-  if (functionType === autoSuggestFunction) {
-    xhr.open('GET', `https://www.alphavantage.co/query?function=${functionType}&keywords=${keyword}&apikey=CPOI5XYGUXDVNA28`);
-    xhr.addEventListener('load', function () {
-      for (var i = 0; i < xhr.response.bestMatches.length; i++) {
-        createAutoSuggestItem(xhr.response.bestMatches[i]['1. symbol']);
-      }
-    });
-  } else {
-    xhr.open('GET', `https://www.alphavantage.co/query?function=${functionType}&symbol=${ticker}&apikey=CPOI5XYGUXDVNA28`);
-    xhr.addEventListener('load', function () {
-    });
-  }
-  xhr.send();
-}
 
-function sendRequestCNBC(requestType, ticker) {
+function sendRequestCNBC(requestType, ticker, input) {
   if (ticker !== null) ticker = ticker.toUpperCase();
   var xhr = new XMLHttpRequest();
   var responseObject;
   xhr.readyState = 'json';
-  if (requestType === trendingStoriesRequest) {
+  if(requestType === autoCompleteRequest) {
+    xhr.open("GET", `https://cnbc.p.rapidapi.com/auto-complete?prefix=${input}`);
+    xhr.addEventListener('load', function(){
+      responseObject = JSON.parse(xhr.response);
+      console.log("response obj", responseObject);
+      createAutoSuggestItem(responseObject);
+    });
+  }else if (requestType === trendingStoriesRequest) {
     xhr.open('GET', 'https://cnbc.p.rapidapi.com/news/list-trending');
-    xhr.setRequestHeader('x-rapidapi-key', 'afbc32455amsh2b70f92ea852178p1d2d81jsn1c3b08275a2e');
-    xhr.setRequestHeader('x-rapidapi-host', 'cnbc.p.rapidapi.com');
     xhr.addEventListener('load', function () {
       responseObject = JSON.parse(xhr.response);
       responseObject = responseObject.data.mostPopular;
+      console.log('CNBC trending', responseObject);
     });
-  } else {
+  }else if(requestType === companyNewsRequest) {
     xhr.open('GET', `https://cnbc.p.rapidapi.com/news/list-by-symbol?tickersymbol=${ticker}&page=1&pagesize=10`);
-    xhr.setRequestHeader('x-rapidapi-key', 'afbc32455amsh2b70f92ea852178p1d2d81jsn1c3b08275a2e');
-    xhr.setRequestHeader('x-rapidapi-host', 'cnbc.p.rapidapi.com');
-    xhr.addEventListener('load', function () {
-      responseObject = JSON.parse(xhr.response);
-      responseObject = responseObject.rss.channel;
-    });
   }
+  xhr.setRequestHeader('x-rapidapi-key', 'afbc32455amsh2b70f92ea852178p1d2d81jsn1c3b08275a2e');
+  xhr.setRequestHeader('x-rapidapi-host', 'cnbc.p.rapidapi.com');
   xhr.send();
 }
 
